e0a16dbf360644bd9dd2962c1ad56ada2b1403ba
Sergii Kashaba <skashaba@mirantis.com>
2013-11-12 16:36:28 -0500

    Fix functional tests running

--- run_tests.py
+++ run_tests.py
@@ -26,16 +26,17 @@ if __name__ == '__main__':
                         help='Do not run pep8')
     parser.add_argument('--no_ut', action='store_true', default=False,
                         help='Do not run UT')
-    parser.add_argument('--no_venv', action='store_false', default=False,
+    parser.add_argument('--no_venv', action='store_true', default=False,
                         help='Do not install venv')
     parser.add_argument('--coverage', action='store_true',
                         help='Generate coverage report')
     parser.add_argument('--functional', action='store_true',  default=False,
                         help='Run functional tests')
+    parser.add_argument('--cst_path', default=None,
+                        help='Run functional tests')
     args = parser.parse_args()
 
     prefix = ''
-
     if not args.no_venv:
         if not os.path.exists(VENV_DIR):
             print 'Create virtual environment'
@@ -62,4 +63,8 @@ if __name__ == '__main__':
         run_cmd(' '.join(ut_cmd))
 
     if args.functional:
-        run_cmd('sct -v -v -f sctfile_ws_macro hello_world.macro_check_hello')
+        cmd = ' '.join((os.path.join(args.cst_path, 'bin/sct'),
+                        '-v -v -f',
+                        os.path.join(args.cst_path, 'sctfile_ws_macro'),
+                        'hello_world.macro_check_hello'))
+        run_cmd(cmd)
